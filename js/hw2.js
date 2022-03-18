3781fc29b25da39ade039d115fabb31f934d2a04
Kyrrah Nork <kyrrahnork@gmail.com>
2022-03-17 17:23:41 -0700

    Add files via upload

---
+++ js/hw2.js
@@ -0,0 +1,6 @@
+let num1 = 10;
+let num2 = 17;
+let total = num1 + num2;
+
+document.getElementById('message').textContent = "Welcome to my other calculator";
+document.getElementById('calc').textContent = "The sum of " + num1 + " + " + num2 + " is : " + total;
